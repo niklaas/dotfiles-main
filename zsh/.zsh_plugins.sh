763f18c10ce99e1c3c2fb2a8c54c0fc780048342
Niklaas Baudet von Gersdorff <stdin@niklaas.eu>
2018-03-27 21:41:47 +0200

    Removes oh-my-zsh git repository because of error
        commant not found: compdef

--- zsh/.zsh_plugins.sh
+++ zsh/.zsh_plugins.sh
@@ -1,4 +1,3 @@
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
 
 source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
