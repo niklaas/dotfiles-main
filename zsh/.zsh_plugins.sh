fe8ff7c358bdae65a0983a03a42e61fdfc666927
Niklaas Baudet von Gersdorff <gersdorff@iz.de>
2018-07-03 13:59:27 +0200

    Gitignores shell file for deploying zsh plugins

--- zsh/.zsh_plugins.sh
+++
@@ -1,6 +0,0 @@
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/common-aliases/common-aliases.plugin.zsh
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
-source /home/niklaas/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
