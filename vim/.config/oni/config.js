bd1e748a427dfec960e9101b04d85733f2f89f26
Niklaas Baudet von Gersdorff <gersdorff@iz.de>
2018-07-26 11:24:19 +0200

    Fixes name for oni config

---
+++ vim/.config/oni/config.js
@@ -0,0 +1,18 @@
+const activate = (oni) => {
+}
+
+const configuration = {
+    activate,
+    "oni.loadInitVim": true,
+    "oni.useDefaultConfig": false,
+
+    "editor.fontSize": "14px",
+
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "ui.colorscheme": "nord",
+
+    "oni.hideMenu": true,
+    "learning.enabled": false,
+    "achievements.enabled": false
+}
