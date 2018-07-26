2b3fda560b3ec8cf7fa08ee7d3d857ce97c54374
Niklaas Baudet von Gersdorff <gersdorff@iz.de>
2018-07-26 12:25:07 +0200

    Uses new config format for oni

--- vim/.config/oni/config.tsx
+++ vim/.config/oni/config.tsx
@@ -1,7 +1,12 @@
-const activate = (oni) => {
+import * as React from "react"
+import * as Oni from "oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+  oni.input.unbind("<c-g>") // make C-g work as expected in vim
+  oni.input.unbind("<c-v>") // make C-v work as expected in vim
 }
 
-const configuration = {
+export const configuration = {
     activate,
     "oni.loadInitVim": true,
     "oni.useDefaultConfig": false,
