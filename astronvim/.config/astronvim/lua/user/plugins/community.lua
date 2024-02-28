4d6fabf8e229244ca6b25e5a0bf7ffdefa29f0f6
Niklaas Baudet von Gersdorff <hello@niklaas.eu>
2024-02-28 15:41:44 +0100

    Add pack for angular

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -1,5 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.ruby" },
-  { import = "astrocommunity.pack.typescript-all-in-one" }
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.angular" }
 }
