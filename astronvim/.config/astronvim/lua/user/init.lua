144e14195a8de8899c8960661b71cd6da9441138
Niklaas Baudet von Gersdorff <hello@niklaas.eu>
2024-03-09 16:03:47 +0100

    Commit astronvim before switching back to vim

--- astronvim/.config/astronvim/lua/user/init.lua
+++ astronvim/.config/astronvim/lua/user/init.lua
@@ -2,7 +2,10 @@ return {
   mappings = {
     n = {
       ["]<space>"] = { "o<esc>k", desc = "New line below" },
-      ["[<space>"] = { "O<esc>j", desc = "New line above" }
-    }
+      ["[<space>"] = { "O<esc>j", desc = "New line above" },
+    },
   },
+  polish = function()
+    vim.o.splitbelow = false
+  end,
 }
--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -1,6 +1,11 @@
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.ruby" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.pack.angular" }
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
+	{ import = "astrocommunity.motion.hop-nvim" },
+	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.pack.angular" },
+	{ import = "astrocommunity.pack.docker" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.ruby" },
+	{ import = "astrocommunity.pack.typescript-all-in-one" },
 }
