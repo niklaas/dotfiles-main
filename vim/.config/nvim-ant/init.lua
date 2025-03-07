b061202c4aae0a9bcc9224358dd635fff9872a98
Niklaas Baudet von Gersdorff <hello@niklaas.eu>
2025-03-07 10:17:04 +0100

    feat(vim): add keymaps to jump to beginning/end of line

--- vim/.config/nvim-ant/init.lua
+++ vim/.config/nvim-ant/init.lua
@@ -40,11 +40,15 @@ map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 map("t", "<c-enter>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+map("n", "gh", "_", { desc = "Jump to beginning of line" })
+map("n", "gl", "$", { desc = "Jump to end of line" })
+
 -- window movement
 map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 map("c", "@@", "@{upstream}", { expr = false })
 
 -- Highlight when yanking (copying) text
