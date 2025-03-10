7fdc3096a645bb77abb5bda1d11dac7c384ab6cc
Niklaas Baudet von Gersdorff <hello@niklaas.eu>
2025-03-10 09:17:56 +0100

    feat(vim): add code companion

---
+++ vim/.config/nvim-ant/lua/plugins/codecompanion.lua
@@ -0,0 +1,78 @@
+local function api_key(filename)
+  return "cmd: gpg --batch --quiet --decrypt " .. vim.fn.stdpath("config") .. "/secrets/" .. filename
+end
+
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
+  },
+  config = true,
+  cmd = { "CodeCompanion", "CodeCompanionChat" },
+  keys = {
+    { "<leader>cc", "<cmd>CodeCompanionChat<cr>", desc = "CodeCompanionChat" },
+    { "<leader>c<space>", ":CodeCompanion /buffer ", desc = "CodeCompanion buffer" },
+  },
+  opts = {
+    window = {
+      position = "right",
+    },
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          schema = {
+            model = {
+              default = "codestral:latest",
+            },
+          },
+        })
+      end,
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key = api_key("anthropic.gpg"),
+          },
+          schema = {
+            model = {
+              default = "claude-3-5-sonnet-20241022",
+            },
+          },
+        })
+      end,
+      mistral = function()
+        return require("codecompanion.adapters").extend("openai_compatible", {
+          name = "mistral",
+          env = {
+            url = "https://api.mistral.ai",
+            api_key = api_key("mistral-ai.gpg"),
+            chat_url = "/v1/chat/completions",
+          },
+          handlers = {
+            form_parameters = function(_, params, _)
+              params.stream_options = nil
+              params.options = nil
+
+              return params
+            end,
+          },
+          schema = {
+            temperature = {
+              default = 0.2,
+              mapping = "parameters", -- not supported in default parameters.options
+            },
+          },
+        })
+      end,
+    },
+    strategies = {
+      chat = {
+        adapter = "ollama",
+      },
+      inline = {
+        adapter = "ollama",
+      },
+    },
+  },
+}
