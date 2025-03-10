a53a7246b554c42fe50269f89ff9447fc6d70005
Niklaas Baudet von Gersdorff <hello@niklaas.eu>
2025-03-10 09:16:50 +0100

    feat(vim): add keybindings for hunks

--- vim/.config/nvim-ant/lua/plugins/gitsigns.lua
+++ vim/.config/nvim-ant/lua/plugins/gitsigns.lua
@@ -10,4 +10,50 @@ return {
       changedelete = { text = "~" },
     },
   },
+  keys = {
+    {
+      "<leader>hh",
+      function()
+        require("gitsigns").stage_hunk()
+      end,
+      desc = "Git [H]unk stage/unstage",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>hr",
+      function()
+        require("gitsigns").reset_hunk()
+      end,
+      desc = "Git [H]unk [r]eset",
+      mode = { "n", "v" },
+    },
+    {
+      "]h",
+      function()
+        require("gitsigns").nav_hunk("next")
+      end,
+      desc = "Next hunk",
+    },
+    {
+      "[h",
+      function()
+        require("gitsigns").nav_hunk("prev")
+      end,
+      desc = "Previous hunk",
+    },
+    {
+      "<leader>hl",
+      function()
+        require("gitsigns").setloclist(0, 0)
+      end,
+      desc = "Git [H]unk [l]oclist",
+    },
+    {
+      "<leader>hq",
+      function()
+        require("gitsigns").setqflist(0)
+      end,
+      desc = "Git [H]unk [q]uickfix list",
+    },
+  },
 }
