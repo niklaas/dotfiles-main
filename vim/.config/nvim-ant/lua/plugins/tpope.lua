d232006d85847c1148b10b140a349f1fd8fb3106
Niklaas Baudet von Gersdorff <hello@niklaas.eu>
2025-03-14 10:10:15 +0100

    feat(vim): add tpope/vim-rails

--- vim/.config/nvim-ant/lua/plugins/tpope.lua
+++ vim/.config/nvim-ant/lua/plugins/tpope.lua
@@ -2,6 +2,7 @@ return {
   { "tpope/vim-dispatch", lazy = false },
   { "tpope/vim-endwise", lazy = false },
   { "tpope/vim-obsession", lazy = false },
+  { "tpope/vim-rails", lazy = false },
   { "tpope/vim-sleuth", lazy = false }, -- detect shiftwidth and tabstop
   { "tpope/vim-unimpaired", event = "VimEnter" }, -- convenient mappings
   { "tpope/vim-vinegar", keys = { "-" } }, -- for better netrw
